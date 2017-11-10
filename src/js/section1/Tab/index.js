a719d2c5feda2f302c1a3f15f3c2a72d73f791d9
Saeki, Takahiro <ts-takahiro.saeki@rakuten.com>
2017-11-10 18:58:44 +0900

    update: changed logic of tab component.

--- src/js/section1/Tab/index.js
+++ src/js/section1/Tab/index.js
@@ -30,19 +30,27 @@ template.innerHTML = `
       outline: none;
       text-align: center;
     }
+    
+    .contents-field ::slotted([disabled-flag="true"]) {
+      display: none;
+    }
   </style>
   <div class="contents-container">
     <ul class="tab-menu-container" menu>
       <slot id="tabsSlot" name="title"></slot>
     </ul>
     <div>contents</div>
-    <div id="tabsContent">
-      <slot id="content" name="content"></slot>
+    <div class="contents-field">
+      <slot id="tabsContent" name="content"></slot>
     </div>
   </div>
 `;
 
 export default class Tab extends HTMLElement {
+  static get observedAttributes() {
+    return ['current-id'];
+  }
+  
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
@@ -52,12 +60,12 @@ export default class Tab extends HTMLElement {
     this.tabsContent = this.shadowRoot.querySelector('#tabsContent');
     this.tabs = this.tabsSlot.assignedNodes({flatten: true});
     this.content = this.tabsContent.assignedNodes({flatten: true});
-    console.log(this.tabs)
-    console.log(this.content)
+
     for (let index in this.tabs) {
       this.tabs[index].setAttribute('id', index)
       if (this.tabs[index].hasAttribute('selected')) {
         this.tabs[index].setAttribute('selected', true)
+        this.setAttribute('current-id', index)
       } else {
         this.tabs[index].setAttribute('selected', false)
       }
@@ -65,6 +73,35 @@ export default class Tab extends HTMLElement {
     
     for (let index in this.content) {
       this.content[index].setAttribute('id', index)
+      if(this.getAttribute('current-id') === index) {
+        this.content[index].setAttribute('disabled-flag', false)
+      } else {
+        this.content[index].setAttribute('disabled-flag', true)
+      }
+    }
+    this._boundOnTitleClick = this._onTitleClick.bind(this);
+    this.tabsSlot.addEventListener('click', this._boundOnTitleClick);
+  }
+  
+  attributeChangedCallback(name, oldValue, newValue) {
+    for (let index in this.content) {
+      this.content[index].setAttribute('id', index)
+      if(this.getAttribute('current-id') === index) {
+        this.content[index].setAttribute('disabled-flag', false)
+      } else {
+        this.content[index].setAttribute('disabled-flag', true)
+      }
+    }
+  }
+  
+  connectedCallback() {
+    
+  }
+  
+  _onTitleClick(e) { 
+    if (e.target.slot === 'title') {
+      this.selected = this.tabs.indexOf(e.target);
+      this.setAttribute('current-id', this.selected)
     }
   }
 }
